ecd627d32f2e2257213327ed90ac9d682193f4ae
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 14:31:50 +0900

    first commit

---
+++ svelte-study/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Life from "./Life.svelte";
+  let isShow = false;
+</script>
+
+{#if isShow}
+  <Life />
+{/if}
+
+<hr>
+<input type="checkbox" bind:checked={isShow}>
+チェックで出るん
---
+++ svelte-study/src/Life.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { beforeUpdate, onMount, onDestroy, afterUpdate } from "svelte";
+  beforeUpdate(() => {
+    console.log("beforeUpdate");
+  });
+  onMount(() => {
+    console.log("onMount");
+  });
+  afterUpdate(() => {
+    console.log("afterUpdate");
+  });
+  onDestroy(() => {
+    console.log("onDestroy");
+  });
+  console.log("Toplevel")
+
+  let text = "テキスト";
+  function updateText() {
+    console.log("updateText関数実行");
+    text = new Date().toLocaleString();
+  }
+</script>
+
+
+<p>Text:{text}</p>
+<button on:click={updateText}>更新</button>
