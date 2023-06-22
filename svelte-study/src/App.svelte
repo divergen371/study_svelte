c1f86bdb87a0e146d100ee4bc8313db7c9dee292
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 15:05:45 +0900

    :alembic: オブジェクトの配列

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-  const messages = ["message1", "message2", "message3"];
+  const messages = [
+    { id: 1, message: "message1" },
+    { id: 2, message: "message2" },
+    { id: 3, message: "message3" },
+  ];
 </script>
 
 {#each messages as message}
-  <p>{message}</p>
+  <p>{message.id}:{message.message}</p>
 {/each}
