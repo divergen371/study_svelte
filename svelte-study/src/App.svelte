41deae628634ddef73c15b9c4fdaf569906da7c7
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 15:07:51 +0900

    :alembic: 空の場合の分岐

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,11 +1,9 @@
 <script>
-  const messages = [
-    { id: 1, message: "message1" },
-    { id: 2, message: "message2" },
-    { id: 3, message: "message3" },
-  ];
+  const messages = [];
 </script>
 
 {#each messages as message}
   <p>{message.id}:{message.message}</p>
+{:else}
+  <p>メッセージはありません</p>
 {/each}
