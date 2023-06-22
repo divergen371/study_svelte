a523259dffbb2971d5c51b171f82c573b20bac0a
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 17:53:23 +0900

    :alembic: テキストエリア

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,13 +1,6 @@
 <script>
-  // あえて値の型と変数名の関連性をあべこべにする
-  let text = 10;
-  let number = "10";
-  function checkType() {
-    console.log(text, typeof text);
-    console.log(number, typeof number);
-  }
+  let note = "";
 </script>
 
-<p>text:<input type="text" bind:value={text} /></p>
-<p>number:<input type="number" bind:value={number} /></p>
-<button on:click={checkType}>型チェック</button>
+<p>note:{note}</p>
+<textarea name="" id="" cols="30" rows="10" bind:value={note}></textarea>
