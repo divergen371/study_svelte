e28fae7c347862616568d68e10fdd4f36e78661b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 16:25:17 +0900

    Promise処理 pending,fulfilled,rejectedで分岐できる

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,19 +1,23 @@
 <script>
-  let items = [
-    { id: 0, name: "要素1" },
-    { id: 1, name: "要素2" },
-    { id: 2, name: "要素3" },
-  ];
-
-  function deleteItem() {
-    const newItems = [...items];
-    newItems.splice(0, 1);
-    items = newItems;
+  async function fetchUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    if (res.ok) {
+      return users;
+    } else {
+      throw new Error(users);
+    }
   }
+
+  let promise = fetchUsers();
 </script>
 
-{#each items as item (item.id)}
-  <p><input type="checkbox" />{item.name}</p>
-{/each}
-
-<button on:click={deleteItem}>削除</button>
+{#await promise}
+  <p>ロード中</p>
+{:then users}
+  {#each users as user}
+    <p>{user.id}:{user.name}</p>
+  {/each}
+{:catch error}
+  <p>Error:{error.message}</p>
+{/await}
