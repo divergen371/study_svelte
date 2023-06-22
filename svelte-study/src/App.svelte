5549a0335a187ac6d45899f855bc33430411701c
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 16:10:15 +0900

    インデックスにkey属性を割り当てる

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   const messages = [
-    { id: 1, message: "message1" },
-    { id: 2, message: "message2" },
-    { id: 3, message: "message3" },
+    { id: 0, message: "message1" },
+    { id: 1, message: "message2" },
+    { id: 2, message: "message3" },
   ];
 </script>
 
-{#each messages as message,i}
-  <p>[No.{i+1}]{message.id}:{message.message}</p>
+{#each messages as message ,i (message.id)}
+  <p>[No {i + 1}]{message.message}</p>
 {/each}
