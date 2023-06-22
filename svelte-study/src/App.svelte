c5dd46e4232a6a522920820ea8cdcfe8d1750c4b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 16:38:57 +0900

    :alembic: フォーム グループ化など

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -1,6 +1,18 @@
 <script>
-  let text = "<a href='https://example.com/'>example.com</a>";
+  let isAgree = false;
+  let vegetables = [];
+  let size = "medium";
 </script>
 
-<p>{text}</p>
-<p>{@html text}</p>
+<input type="checkbox" name="" id="" bind:checked={isAgree} />利用規約に同意する
+<input type="button" value="次へ" disabled={!isAgree} />
+<hr />
+<p>vegetables:{vegetables}</p>
+<input type="checkbox" value="tomato" bind:group={vegetables} />tomato
+<input type="checkbox" value="carrot" bind:group={vegetables} />carrot
+<input type="checkbox" value="onion" bind:group={vegetables} />onion
+<hr />
+<p>size:{size}</p>
+<input type="radio" value="large" bind:group={size}>large
+<input type="radio" value="medium" bind:group={size}>medium
+<input type="radio" value="small" bind:group={size}>small
